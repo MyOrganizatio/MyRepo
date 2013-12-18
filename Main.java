3f673b8b67472500563de5fde81fac251d060752
Daniel <daniyanes7@gmail.co>
2013-12-18 11:07:49 +0100

    First Commit to Master
    We performed the first commit in the master brach

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.pruebaGit.main;
+
+public class Main {
+
+	public static int main(String[] args) {
+		System.out.print("Hello world!! ");
+		
+		System.out.print("you heard me?? ");
+		return 0;
+	}
+
+}
